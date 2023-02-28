941df7898b2b766db8e41150f00b0763aadc890d
stormstina <stina.nerin@gmail.com>
2023-02-28 11:01:36 +0100

    Setup tamagotchi prototype

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 console.log("tamagotchi");
 
+class Tamagotchi {
+    constructor(name, animalType ) {
+        this.name = name;
+        this.animaType = animalType;
+        this.tiredness = 50;
+        this.hunger = 50;
+        this.loneliness = 50;
+        this.happiness = 50;
+    }
+    nap() {
+        /*  
+            Sänker tiredness med 40,
+            sänker happiness med 10, 
+            ökar hunger och loneliness med 10.
+        */
+
+    }
+    play() {
+        /*
+            Ökar happiness med 30, 
+            ökar hunger och tiredness med 20. 
+            Sänker loneliness med 10.
+        */
+
+    }
+    eat() {
+        /*
+            Sänker hunger med 60, 
+            ökar tiredness med 10.
+        */
+
+    }
+}
